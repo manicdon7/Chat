4915473cc348369bef141dbadc7f383018b96389
Mani kandan <manicdon7@example.com>
2023-05-13 20:59:37 +0530

    TD

--- Chat/Chatapp/forms.py
+++
@@ -1,8 +0,0 @@
-# chat/forms.py
-from django import forms
-
-class SendMessageForm(forms.Form):
-    message_text = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control',
-        'placeholder': 'Type your message here...',
-    }))
--- Chat/Chatapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('/',views.index,name='Index'),
-    path('friend/<str"pk>',views.detail,name="detail")
-]
