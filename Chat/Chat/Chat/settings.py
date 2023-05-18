1a773d202ef45ff888aa8765d07a93b8db817ff3
Mani kandan <manicdon7@example.com>
2023-05-18 21:23:56 +0530

    FD

--- Chat/Chat/Chat/settings.py
+++ Chat/Chat/Chat/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mychatapp'
+    'Chatapp'
 ]
 
 MIDDLEWARE = [
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mychat.urls'
+ROOT_URLCONF = 'Chatapp.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mychat.wsgi.application'
+WSGI_APPLICATION = 'Chatapp.wsgi.application'
 
 
 # Database
